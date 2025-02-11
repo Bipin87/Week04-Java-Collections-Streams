5aa1a0dff1b6e78ca04f6f76b5a59a0e64ebdb0b
Bipin87 <sahubipin484@gmail.com>
2025-02-11 11:53:46 +0530

    [Bipin Kumar Sahu] Add: Test the smart warehouse management system code✨

---
+++ Day-01/src/test/java/com/capgemini/java_generics/smart_warehouse_management_system/TestGenerics.java
@@ -0,0 +1,13 @@
+package com.capgemini.java_generics.smart_warehouse_management_system;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestGenerics {
+    @Test
+    public void test(){
+
+    }
+
+}
