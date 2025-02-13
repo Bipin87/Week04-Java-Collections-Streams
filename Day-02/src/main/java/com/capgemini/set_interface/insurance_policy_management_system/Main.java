9d01c695ccf5e5a1354a0530bccce8c7e9705e78
Bipin87 <sahubipin484@gmail.com>
2025-02-13 11:40:58 +0530

    [Bipin Kumar Sahu] Add: Day 02 Collections code done

--- Day-02/src/main/java/com/capgemini/set_interface/insurance_policy_management_system/Main.java
+++ Day-02/src/main/java/com/capgemini/set_interface/insurance_policy_management_system/Main.java
@@ -10,7 +10,7 @@ public class Main {
         system.addPolicy(new InsurancePolicy("P111", "Abhinav", new Date(System.currentTimeMillis() + 12000000L * 10), "Health", 499.99));
         system.addPolicy(new InsurancePolicy("P112", "BK Sahu", new Date(System.currentTimeMillis() + 24000000L * 40), "Auto", 60.0));
         system.addPolicy(new InsurancePolicy("P113", "Deepraj", new Date(System.currentTimeMillis() + 86400000L * 20), "Home", 900.0));
-        system.addPolicy(new InsurancePolicy("P123", "Aman", new Date(System.currentTimeMillis() + 57000000L * 10), "Health", 500.0));
+        system.addPolicy(new InsurancePolicy("P123", "Aryan", new Date(System.currentTimeMillis() + 57000000L * 10), "Health", 500.0));
 
         // Display results
         system.displayAllPolicies();
