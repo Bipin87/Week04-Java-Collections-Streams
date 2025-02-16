5a8ba3802bde668dd5b3c4e78b172f234ec75541
Bipin87 <sahubipin484@gmail.com>
2025-02-17 00:29:43 +0530

    [Bipin Kumar Sahu] Add: Test perform in @BeforeEach and @AfterEach annotaions code file added🦠

---
+++ Day-05/src/test/java/com/capgemini/junit/beforeeach_and_aftereach_annotations/DatabaseConnectionTest.java
@@ -0,0 +1,39 @@
+package com.capgemini.junit.beforeeach_and_aftereach_annotations;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionTest {
+    private DatabaseConnection dbConnection;
+
+    @BeforeEach
+    void setUp() {
+        dbConnection = new DatabaseConnection();
+        dbConnection.connect();
+        assertTrue(dbConnection.isConnected());
+    }
+
+    @Test
+    void testConnectionEstablished() {
+        assertTrue(dbConnection.isConnected());
+        System.out.println("Test1 Passed");
+    }
+
+    @Test
+    void testDisconnect() {
+        dbConnection.disconnect();
+        assertFalse(dbConnection.isConnected());
+        System.out.println("Test2 Passed");
+    }
+
+    @AfterEach
+    void tearDown() {
+        dbConnection.disconnect();
+        assertFalse(dbConnection.isConnected());
+        System.out.println("Test3 Passed After each test");
+    }
+
+}
