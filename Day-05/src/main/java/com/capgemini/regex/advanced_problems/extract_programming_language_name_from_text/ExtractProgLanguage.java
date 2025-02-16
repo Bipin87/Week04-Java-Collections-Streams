77bd51d7c53c97606c90730153664a5c902842b2
Bipin87 <sahubipin484@gmail.com>
2025-02-16 22:34:06 +0530

    [Bipin Kumar Sahu] Add: extract programming language in text code🦠

---
+++ Day-05/src/main/java/com/capgemini/regex/advanced_problems/extract_programming_language_name_from_text/ExtractProgLanguage.java
@@ -0,0 +1,26 @@
+package com.capgemini.regex.advanced_problems.extract_programming_language_name_from_text;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractProgLanguage {
+    // Method to extract the programming language
+    public static String extractProgrammingLanguage(String text){
+        String result = "";
+        String regex = "Java|Python|JavaScript|Go";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while(matcher.find()){
+            result += matcher.group() + " ";
+        }
+        String[] arr = result.split(" ");
+        result = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length - 1) result += (arr[i] + ", ");
+            else result += arr[i];
+        }
+        return result;
+    }
+}
